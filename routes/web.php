a245fd0ac08ba7e9da43d3a4478d2d507bb12899
X-DX <aruproy5637@gmail.com>
2024-11-23 20:14:23 +0530

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,19 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/login',[LoginController::class,'index'])->name('account.login');
+Route::group(['prefix' => 'account'],function(){
+    Route::group(['middleware' => 'guest'],function() {
+        Route::get('/account/login',[LoginController::class,'index'])->name('account.login');
+        Route::get('/account/register',[LoginController::class,'register'])->name('account.register');
+        Route::post('/account/process-register',[LoginController::class,'processRegister'])->name('account.processRegister');
+        Route::post('/account/authenticate',[LoginController::class,'authenticate'])->name('account.authenticate');
+    });
+
+    Route::group(['middleware' => 'auth'], function(){
+        Route::get('/account/logout',[LoginController::class,'logout'])->name('account.logout');
+        Route::get('/account/dashboard',[DashboardController::class,'index'])->name('account.dashboard');
+    });
+});
+
+
+
